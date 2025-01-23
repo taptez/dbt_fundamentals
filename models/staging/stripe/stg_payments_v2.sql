2f9b907222c10e5599c6eb336b9b48543111cabf
William ZOUNON <william.zounon@artik-consulting.com>
2025-01-23 16:01:19 +0100

    learn model version

--- models/staging/stripe/stg_payments_v2.sql
+++ models/staging/stripe/stg_payments_v2.sql
@@ -5,7 +5,7 @@ select
     paymentmethod as payment_method,
     status,
     -- amount is stored in cents, convert it to dollars
-    amount / 100 as amount,
-    created as created_at
+    amount / 100 as payment_amount,
+    cast(created as date) as created_at
 
 from {{ source('stripe', 'payment') }}

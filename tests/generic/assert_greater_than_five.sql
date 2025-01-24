509139c35750a9867693c682cf8e532d31e7426e
William ZOUNON <william.zounon@artik-consulting.com>
2025-01-24 16:40:11 +0100

    Learn advanced test

---
+++ tests/generic/assert_greater_than_five.sql
@@ -0,0 +1,15 @@
+
+{{
+    config (
+        severity='warn'
+    )
+}}
+
+{% test greater_than_five(model, column_name) %}
+
+select
+    {{ column_name }}
+from {{ model }}
+where {{ column_name }} <= 5
+
+{% endtest %}
